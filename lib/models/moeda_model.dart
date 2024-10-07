41c7711a1b8554943268bd59543b504a8d5f6123
Keidson Roby <keidsondesigner@gmail.com>
2024-10-07 18:05:27 -0400

    feat: Criado MoedaModel

---
+++ lib/models/moeda_model.dart
@@ -0,0 +1,12 @@
+class MoedaModel {
+  String icone;
+  String nome;
+  String sigla;
+  double preco;
+
+  MoedaModel(
+      {required this.icone,
+      required this.nome,
+      required this.sigla,
+      required this.preco});
+}

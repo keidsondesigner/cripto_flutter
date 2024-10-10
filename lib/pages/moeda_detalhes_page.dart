9212b1572e973a8d68dbe036d77bb924adc92f0f
Keidson Roby <keidsondesigner@gmail.com>
2024-10-09 23:30:23 -0400

    feat: Ajuste d estilos no MoedaDetalhes

--- lib/pages/moeda_detalhes_page.dart
+++ lib/pages/moeda_detalhes_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:teste_flutter/models/moeda_model.dart';
 
 class MoedaDetalhesPage extends StatefulWidget {
@@ -11,24 +12,55 @@ class MoedaDetalhesPage extends StatefulWidget {
 }
 
 class _MoedaDetalhesPageState extends State<MoedaDetalhesPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.moeda.nome),
+          title: Row(
+            mainAxisAlignment:
+                MainAxisAlignment.end, // Alinhando o texto à direita
+            children: [
+              Text(widget.moeda.nome),
+            ],
+          ),
+          titleTextStyle: const TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          ),
         ),
-        body: Column(
-          children: [
-            Row(
+        body: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
               children: [
-                SizedBox(
-                  child: Image.asset(widget.moeda.icone),
-                  width: 50,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 50,
+                            child: Image.asset(widget.moeda.icone),
+                          ),
+                          Container(width: 16),
+                          Text(real.format(widget.moeda.preco),
+                              style: const TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black87,
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text(widget.moeda.preco.toString()),
               ],
-            ),
-          ],
-        ));
+            )));
   }
 }
